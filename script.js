55c1629411cb7f0fe71bbf9d69747f46cfcb88f3
abhishek <abhishekjaswal1122@gmail.com>
2025-04-23 15:49:01 +0530

    added

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ loadDqata()
 /* for each:- performs an operation foe each element but doestnt return anything
 map:- performs an operation for each element and return a new array
  */
+
+//for each() performs an operation for each element but doest'n return anyrthing 
+// map() performs  a transformation and return a new array.
+
+const nums = [1 , 2 , 3];
+
+const doubled = nums.map(n => n * 2);
+nums.forEach(n=> console.log(n*2));
